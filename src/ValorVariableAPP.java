7cb1ff48b6878d176c8846f7227d7a8d1f11a644
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-12 00:45:02 +0100

    PRIMER COMMIT

---
+++ src/ValorVariableAPP.java
@@ -0,0 +1,19 @@
+
+public class ValorVariableAPP {
+
+	public static void main(String[] args) {
+		
+		int variableN = 5;
+		double variableA = 4.56;
+		char variableC = 'a';
+		int valorNumericoC = variableC;
+		
+		System.out.println("Variable N es = " + variableN);
+		System.out.println("Variable A es = " + variableA);
+		System.out.println("Variable C es = " + variableC);
+		System.out.println("5 + 4.56 = " + (variableN + variableA));
+		System.out.println("4.56 + 5 =" + (variableN - variableA));
+		System.out.println("Valor numérico del carácter a es = " + valorNumericoC);
+		
+	}
+}
